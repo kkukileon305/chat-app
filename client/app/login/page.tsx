f9bb19c94d262cc6f4abd1654045dd1c4b2d82f0
kkukileon305 <kkukileon305@gmail.com>
2022-11-28 00:07:48 +0900

    client socket 연결

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,4 +1,26 @@
+'use client';
+
+import { useEffect } from 'react';
+import { io } from 'socket.io-client';
+
+const socket = io({
+  autoConnect: false,
+  path: 'http://localhost:4000',
+});
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  useEffect(() => {
+    socket.connect();
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  return (
+    <div>
+      <button>goodReq 이벤트 발생</button>
+    </div>
+  );
 };
 export default LoginPage;
